a6bc69b91c94628e5f015dce698fd9688bc436a2
Philipe Vieira <philipefilho@hotmail.com>
2020-04-21 00:25:20 -0300

    criação de seeds de teste

---
+++ src/database/seeds/2_business.js
@@ -0,0 +1,29 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('business').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('business').insert([
+        {
+          id: 1, 
+          businessTitle: 'Business Example', 
+          description: 'this is a business example', 
+          phone: '00 0000-0000', 
+          street: null, 
+          neighborhood: null, 
+          coordinates: null, 
+          userId: 1
+        },
+        {
+          id: 2, 
+          businessTitle: 'Business Example 2', 
+          description: 'this is a business example', 
+          phone: '00 0000-0000', 
+          street: null, 
+          neighborhood: null, 
+          coordinates: null, 
+          userId: 2},
+      ]);
+    });
+};
