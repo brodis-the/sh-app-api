3b72963bd451a5282461e7e0c12db43c9a91e3cc
Philipe Vieira <philipefilho@hotmail.com>
2020-04-22 02:05:09 -0300

    criação do CRUD de usuário

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,13 @@ Route.get('/', (request, response) => {
       status: 'OK!'
   });
 });
+
 Route.get('/users', UserController.index )
+Route.get('/users/:id', UserController.show )
+Route.post('/users', UserController.store )
+Route.patch('/users/:id', UserController.update )
+Route.delete('/users/:id', UserController.destroy )
+
 Route.get('/business', BusinessController.index )
 
 module.exports = Route;
