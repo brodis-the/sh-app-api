aedccaf4ac7de2c03c52d8503c889eac4e0a4da3
Philipe Vieira <philipefilho@hotmail.com>
2020-05-03 21:51:05 -0300

    adicionando o campo senha para usuários

--- src/middlewares/UserMiddleware.js
+++ src/middlewares/UserMiddleware.js
@@ -8,24 +8,38 @@ const show = [
   ]
 
 const store = [
-    check('name').notEmpty().withMessage('name field is required').bail()
-      .isString().withMessage('name field needs to be string').bail()
-      .isAlpha('pt-BR').withMessage(' name field needs to be alphabetic'),
+  check('name').notEmpty().withMessage('name field is required').bail()
+    .isString().withMessage('name field needs to be string').bail()
+    .isAlpha('pt-BR').withMessage(' name field needs to be alphabetic'),
 
-    check('cpf').notEmpty().withMessage('cpf field is required').bail()
-      .isString().withMessage('cpf field needs to be string').bail()
-      .isWhitelisted('0123456789.-').withMessage('cpf field must contain only numbers').bail()
-      .isLength({min:11, max: 14}).withMessage('cpf length is invalid'),
+  check('cpf')
+    .isString().withMessage('cpf field needs to be string').bail()
+    .isWhitelisted('0123456789.-').withMessage('cpf field must contain only numbers').bail()
+    .isLength({min:11, max: 14}).withMessage('cpf length is invalid')
+    .optional({ nullable: true }),
 
-    check('email').notEmpty().withMessage('email field is required').bail()
-      .isEmail().withMessage('is not a valid email'),
+  check('email').notEmpty().withMessage('email field is required').bail()
+    .isEmail().withMessage('is not a valid email'),
 
-    check('phone').notEmpty().withMessage('phone field is required').bail()
-      .isWhitelisted('0123456789+- ').withMessage('phone field must contain only numbers').bail()
-      .isLength({min: 8, max: 17}).withMessage('phone length is invalid'),
-      
-    showValidation,
-  ]
+  check('password').notEmpty().withMessage('password field is required').bail()
+    .isString().withMessage('password field needs to be string').bail(),
+  
+  check('passwordConfirmation').notEmpty().withMessage('passwordConfirmation field is required').bail()
+    .isString().withMessage('password field needs to be string').bail()
+    .custom((value, { req })=>{
+      if (value != req.body.password) {
+        return Promise.reject('Password confirmation does not match password');
+      }
+      return true
+    }),
+
+  check('phone')
+    .isWhitelisted('0123456789+- ').withMessage('phone field must contain only numbers').bail()
+    .isLength({min: 8, max: 17}).withMessage('phone length is invalid')
+    .optional({ nullable: true }),
+    
+  showValidation,
+]
 
 const update = [
     param('id')
--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -2,7 +2,7 @@ const { validationResult } = require('express-validator')
 
 const showValidation = (req, res, next) => {
   if (!validationResult(req).isEmpty()){
-    return res.status(400).json({ errors: validationResult(req).array()});
+    return res.status(400).json({ errors: validationResult(req).array()})
   } 
   next()
 }
