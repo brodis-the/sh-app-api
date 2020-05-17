3d0ea95b1e38403c69dc67dc06f2b114016a5f7b
Philipe Vieira <philipefilho@hotmail.com>
2020-05-16 22:35:19 -0300

    Atualizando os seeds

--- src/database/seeds/1_users.js
+++ src/database/seeds/1_users.js
@@ -1,4 +1,4 @@
-const {v4: uuidv4} = require('uuid')
+const crypto = require('crypto')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -7,30 +7,33 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').insert([
         {
-          id: uuidv4(),
+          id: '77b37574-cc54-433a-8d65-009e74778b65',
           name: 'Joao', 
           nickname: 'Joao',
           cpf: '000.000.000-00', 
           email: 'joao@email.com', 
-          password: '123456', 
+          emailVerifiedAt: '2020-05-17T01:06:22.319Z',
+          password: crypto.createHash('sha256').update('123456').digest('base64'), 
           phone: '00 0000-0000'
         },
         {
-          id: uuidv4(),
+          id: '3dd2f58d-4c19-4f86-8f1a-f23dc2a30d64',
           name: 'Valeria', 
           nickname: 'valeria',
           cpf: '111.111.111-11', 
           email: 'valeria@email.com', 
-          password: '123456', 
+          emailVerifiedAt: '2020-05-20T01:06:22.323Z',
+          password: crypto.createHash('sha256').update('123456').digest('base64'),  
           phone: '11 1111-1111'
         },
         {
-          id: uuidv4(),
+          id: '31328235-aad0-463a-a9c8-05d09c7db6ec',
           name: 'Maria', 
           nickname: 'maria',
           cpf: '222.222.222-22', 
           email: 'maria@email.com', 
-          password: '123456', 
+          emailVerifiedAt: null,
+          password: crypto.createHash('sha256').update('123456').digest('base64'), 
           phone: '22 2222-2222'
         },
       ]);
--- src/database/seeds/2_business.js
+++ src/database/seeds/2_business.js
@@ -8,23 +8,47 @@ exports.seed = function(knex) {
       return knex('business').insert([
         {
           id: uuidv4(),
-          businessTitle: 'Business Example', 
+          businessTitle: 'Carpintaria de José', 
           description: 'this is a business example', 
           phone: '00 0000-0000', 
-          street: null, 
-          neighborhood: null, 
+          street: 'São Joao Paulo II', 
+          neighborhood: 'Shalom da Paz', 
+          zipCode: '65636330',
           coordinates: null, 
-          userId: 1
+          userId: '77b37574-cc54-433a-8d65-009e74778b65'
         },
         {
           id: uuidv4(),
-          businessTitle: 'Business Example 2', 
+          businessTitle: 'Salão de Beleza das Marias', 
           description: 'this is a business example', 
-          phone: '00 0000-0000', 
+          phone: '11 1111-1111', 
+          street: 'Manoel Nogueira Lima, 144', 
+          neighborhood: 'Joquei', 
+          zipCode: '65636330',
+          coordinates: `5°04'37.5"S 42°47'16.9"W`, 
+          userId: '77b37574-cc54-433a-8d65-009e74778b65'
+        },
+        {
+          id: uuidv4(),
+          businessTitle: 'Lanchonete da Paz', 
+          description: null, 
+          phone: '22 2222-2222', 
           street: null, 
           neighborhood: null, 
+          zipCode: null,
           coordinates: null, 
-          userId: 2
+          userId: '77b37574-cc54-433a-8d65-009e74778b65'
+        },
+        {
+          id: uuidv4(),
+          businessTitle: 'Casa do TI', 
+          description: 'Local onde é escrito muito código', 
+          phone: '33 3333-3333', 
+          street: 'VsCode, 256', 
+          neighborhood: 'CPU', 
+          zipCode: '01024-128',
+          coordinates: `-5.034679, -42.839689`, 
+          userId: '3dd2f58d-4c19-4f86-8f1a-f23dc2a30d64'
         },
       ]);
     });
